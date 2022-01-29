9f60648f4e43f42041c10a3dc5b2abee07fe100a
David Grammatico <david.lugdunum@gmail.com>
2022-01-29 14:43:38 +0100

    finish user liste

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/globals.scss'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Container from '../components/Container/Container';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter()
+
+  console.log(router.route);
   return (
+    
     <Container>
       <Component {...pageProps} />
     </Container>
--- pages/users/[user].js
+++ pages/users/[user].js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function user(props) {
-  console.log(props.user);
+
   return (
     <div className="container px-4 pt-3">
       <h1 className="text-center mb-4">
